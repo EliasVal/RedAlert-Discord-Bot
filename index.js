8b942245e1f47fbccb6c63497a49127f2f9a0d68
EliasVal <46247153+EliasVal@users.noreply.github.com>
2023-10-16 09:47:27 +0300

    Full bot

---
+++ index.js
@@ -0,0 +1,69 @@
+import { Client, GatewayIntentBits, EmbedBuilder } from 'discord.js';
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+import fetch from 'node-fetch';
+
+import config from 'dotenv';
+config();
+
+let lastAlertId = -1;
+
+const cities = (await (await fetch('https://www.tzevaadom.co.il/static/cities.json?v=5')).json()).cities;
+
+const threats = {
+  0: 'Red Alert',
+  1: 'Hazardous Materials Incident',
+  2: 'Suspected Terrorist infiltration',
+  3: 'Earthquake',
+  4: 'Suspected Tsunami',
+  5: 'Hostile aircraft intrusion',
+  6: 'Suspected Radiological incident',
+  7: 'Non-conventional missile',
+  8: 'Alert',
+  9: 'Red Alert Drill',
+};
+
+client.on('ready', () => {
+  setInterval(async () => {
+    const res = await fetch('https://api.tzevaadom.co.il/alerts-history/');
+
+    let allAlerts;
+    try {
+      allAlerts = await res.json();
+    } catch {
+      allAlerts = [{ id: -1 }];
+    }
+
+    if (allAlerts[0].id != lastAlertId) {
+      if (lastAlertId != -1) {
+        const alerts = allAlerts.filter((alert) => alert.id > lastAlertId);
+
+        let msg = '';
+
+        for (const alert of alerts) {
+          for (const a of alert.alerts) {
+            if (a.threat == 9) continue;
+            if (a.isDrill) continue;
+
+            msg += `${threats[a.threat]} - [${new Date(a.time * 1000).toLocaleTimeString('he-IL')}]:\n${a.cities
+              .map((city) => cities[city].en)
+              .join(', ')}\n\n`;
+          }
+
+          const c = await client.channels.fetch('1163247036808839259');
+
+          const embed = new EmbedBuilder();
+          embed.setColor('DarkRed');
+          embed.setTitle('Sirens in Israel');
+          embed.setDescription(msg);
+          c.send({ embeds: [embed] });
+        }
+      }
+
+      lastAlertId = allAlerts[0].id;
+    }
+  }, 2500);
+});
+
+client.login(process.env.TOKEN);
+
+export {};
